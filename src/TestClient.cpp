eca5e6b9fbdd9c775835628ed305abf5e4f12dee
kang <hust.wangkang@gmail.com>
2025-06-06 23:33:21 +0800

    优化了一下写法

--- src/TestClient.cpp
+++ src/TestClient.cpp
@@ -1,59 +1,44 @@
 #include "TestClient.h"
 
 void TestClient::TimeConsumingCall() {
-  TimeConsumingRequest request;
-  TimeConsumingResponse response;
-  ::grpc::ClientContext context;
   std::mutex mu;
   std::condition_variable cv;
-  bool done1 = false;
-  bool done2 = false;
-  bool done3 = false;
+  std::vector<bool> results;
 
   std::cout << "【" << std::this_thread::get_id() << "】" << "before calling【TimeConsumingMethod】" << std::endl;
-  stub_->async()->TimeConsumingMethod(&context, &request, &response, [&](::grpc::Status status) {
-    auto res = response.status();
-    std::cout << "【" << std::this_thread::get_id() << "】" << "in the callback function, status is " << res
-              << std::endl;
-    std::lock_guard<std::mutex> lock(mu);
-    done1 = true;  // 保证了wait的正常返回
-    cv.notify_one();
-    std::cout << "【" << std::this_thread::get_id() << "】" << "callback done" << std::endl;
-  });
+  for (int i = 0; i < 5; ++i) {
+    TimeConsumingRequest request;
+    TimeConsumingResponse response;
+    ::grpc::ClientContext context;
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  TimeConsumingRequest request2;
-  TimeConsumingResponse response2;
-  ::grpc::ClientContext context2;
-  stub_->async()->TimeConsumingMethod(&context2, &request2, &response2, [&](::grpc::Status status) {
-    auto res = response2.status();
-    std::cout << "【" << std::this_thread::get_id() << "】" << "in the callback function, status is " << res
-              << std::endl;
-    std::lock_guard<std::mutex> lock(mu);
-    done2 = true;  // 保证了wait的正常返回
-    cv.notify_one();
-    std::cout << "【" << std::this_thread::get_id() << "】" << "callback done" << std::endl;
-  });
+    int index = results.size();
+    results.push_back(false);
+    stub_->async()->TimeConsumingMethod(
+        &context, &request, &response, [&results, &response, &mu, &cv, index](::grpc::Status status) {
+          auto res = response.status();
+          std::cout << "【" << std::this_thread::get_id() << "】" << "in the callback function, status is " << res
+                    << std::endl;
+          std::lock_guard<std::mutex> lock(mu);
+          results[index] = true;  // 保证了wait的正常返回
+          cv.notify_one();
+          std::cout << "【" << std::this_thread::get_id() << "】" << "callback done" << std::endl;
+        });
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  TimeConsumingRequest request3;
-  TimeConsumingResponse response3;
-  ::grpc::ClientContext context3;
-  stub_->async()->TimeConsumingMethod(&context3, &request3, &response3, [&](::grpc::Status status) {
-    auto res = response3.status();
-    std::cout << "【" << std::this_thread::get_id() << "】" << "in the callback function, status is " << res
-              << std::endl;
-    std::lock_guard<std::mutex> lock(mu);
-    done3 = true;  // 保证了wait的正常返回
-    cv.notify_one();
-    std::cout << "【" << std::this_thread::get_id() << "】" << "callback done" << std::endl;
-  });
   std::cout << "【" << std::this_thread::get_id() << "】"
             << "after calling【TimeConsumingMethod】, waiting for call return" << std::endl;
   //   std::this_thread::sleep_for(std::chrono::seconds(8));
   std::unique_lock<std::mutex> lock(mu);
-  cv.wait(lock, [&done1, &done2, &done3] {
-    if ((done1 && done2 && done3) == false) { std::cout << "not all done" << std::endl; }
-    return done1 && done2 && done3;
+
+  cv.wait(lock, [&results] {
+    bool all_true = std::all_of(results.begin(), results.end(), [](bool b) { return b; });
+    if (all_true == false) {
+      std::cout << "not all done" << std::endl;
+    }  // 这里可以打印出当前的results状态
+    else {
+      std::cout << "all done" << std::endl;
+    }
+    return all_true;
   });  // 这个done很重要，保证了wait就算在notify之后被调用，也能正常返回。
 }
